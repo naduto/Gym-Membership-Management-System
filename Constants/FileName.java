4aaa996ea3af57f36c704e1f4207b5942739e9ac
naduto <145576983+naduto@users.noreply.github.com>
2025-03-18 21:23:19 +0200

    Adding constants interfaces

---
+++ Constants/FileName.java
@@ -0,0 +1,10 @@
+
+package Constants;
+
+public interface FileName {
+    String TRAINER_FILENAME = "Trainer.txt"; 
+    String MEMBER_FILENAME = "Member.txt"; 
+    String CLASS_FILENAME = "Class.txt"; 
+    String REGISTRATION_FILENAME = "Registration.txt";
+    
+}
